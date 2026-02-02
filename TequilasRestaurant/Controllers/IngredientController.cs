ba41c63c6826f9fc3f26851114ed6cbb14dba615
Luke Epperson <lepperson11@gmail.com>
2026-02-02 14:57:18 -0700

    Added the Menu

--- TequilasRestaurant/Controllers/IngredientController.cs
+++ TequilasRestaurant/Controllers/IngredientController.cs
@@ -46,5 +46,31 @@ namespace TequilasRestaurant.Controllers
         {
             return View(await ingredients.GetByIdAsync(id, new QueryOptions<Ingredient> { Includes = "ProductIngredients.Product" }));
         }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Delete(Ingredient ingredient)
+        {
+            await ingredients.DeleteAsync(ingredient.IngredientId);
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Edit(int id)
+        {
+            return View(await ingredients.GetByIdAsync(id, new QueryOptions<Ingredient> { Includes = "ProductIngredients.Product" }));
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(Ingredient ingredient)
+        {
+            if (ModelState.IsValid)
+            {
+                await ingredients.UpdateAsnyc(ingredient);
+                return RedirectToAction("Index");
+            }
+            return View(ingredient);
+        }
     }
 }
---
+++ TequilasRestaurant/Controllers/ProductController.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TequilasRestaurant.Data;
+using TequilasRestaurant.Models;
+
+namespace TequilasRestaurant.Controllers
+{
+    public class ProductController : Controller
+    {
+        private Repository<Product> products;
+        private Repository<Ingredient> ingredients;
+        private Repository<Category> categories;
+        private readonly IWebHostEnvironment _webHostEnviroment;
+
+        public ProductController(ApplicationDbContext context, IWebHostEnvironment webHostEnviroment)
+        {
+            products = new Repository<Product>(context);
+            ingredients = new Repository<Ingredient>(context);
+            categories = new Repository<Category>(context);
+            _webHostEnviroment = webHostEnviroment;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            return View(await products.GetAllAsync());
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> AddEdit(int id)
+        {
+            ViewBag.Ingredients = await ingredients.GetAllAsync();
+            ViewBag.Categories = await categories.GetAllAsync();
+            if (id == 0)
+            {
+                ViewBag.Operation = "Add";
+                return View(new Product());
+            }
+            else
+            {
+                Product product = await products.GetByIdAsync(id, new QueryOptions<Product>
+                {
+                    Includes = "ProductIngredients.Ingredient, Category"
+                });
+                ViewBag.Operation = "Edit";
+                return View(product);
+            }
+        }
+        [HttpPost]
+        public async Task<IActionResult> AddEdit(Product product, int[] ingredientIds, int catId)
+        {
+            if (ModelState.IsValid)
+            {
+                if (product.ImageFile != null)
+                {
+                    string uploadsFolder = Path.Combine(_webHostEnviroment.WebRootPath, "images");
+                    string uniqueFileName = Guid.NewGuid().ToString() + "_" + product.ImageFile.FileName;
+                    string filePath = Path.Combine(uploadsFolder, uniqueFileName);
+                    using (var fileStream = new FileStream(filePath, FileMode.Create))
+                    {
+                        await product.ImageFile.CopyToAsync(fileStream);
+                    }
+                    product.ImageUrl = uniqueFileName;
+                }
+
+                if(product.ProductId == 0)
+                {
+                    ViewBag.Ingredients = await ingredients.GetAllAsync();
+                    ViewBag.Categories = await categories.GetAllAsync();
+                    product.CategoryId = catId;
+
+                    foreach (int id in ingredientIds)
+                    {
+                        product.ProductIngredients?.Add(new ProductIngredient { IngredientId = id, ProductId = product.ProductId });
+                    }
+
+                    await products.AddAsync(product);
+                    return RedirectToAction("Index", "Product");
+                }
+                else
+                {
+                    return RedirectToAction("Index", "Product");
+                }
+            }
+            else
+            {
+                return View(product);
+            }
+        }
+    }
+}
